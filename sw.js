6d45dcab0bfcafc8f5044658918eae7a45c30a3d
SuperMat <mathieu.cavert@gmail.com>
2026-09-16 22:18:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Cache hors ligne de l'application.
    IMPORTANT : après chaque modification de index.html, changez VERSION
    (ex. 'echo-v2') pour que les utilisateurs reçoivent la mise à jour. */
-const VERSION = 'echo-v17';
+const VERSION = 'echo-v18';
 const CORE = [
   './',
   './index.html',
